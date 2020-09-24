0729acc933fdbd45a39be1fea4467b6f74c5836a
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2020-09-24 21:57:44 +0200

    Add /api/images

--- code/album-server/src/api.rs
+++ code/album-server/src/api.rs
@@ -1,11 +1,25 @@
 use rocket::{get, routes, Route};
 use rocket_contrib::json::Json;
+use serde::Serialize;
+
+#[derive(Debug, Serialize)]
+struct Image {
+    filename: String,
+}
+
+#[derive(Debug, Default, Serialize)]
+struct Images(Vec<Image>);
 
 pub fn routes() -> Vec<Route> {
-    routes![index]
+    routes![index, images]
 }
 
 #[get("/")]
 fn index() -> Json<String> {
     Json("hello".to_string())
+}
+
+#[get("/images")]
+fn images() -> Json<Images> {
+    Json(Images::default())
 }
