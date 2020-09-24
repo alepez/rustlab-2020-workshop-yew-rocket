ab00ff6abf1b3104844663389375ac1c3afb3f65
Alessandro Pezzato <alessandro.pezzato@gmail.com>
2020-09-24 23:15:21 +0200

    Create album-db crate

---
+++ code/album-db/src/lib.rs
@@ -0,0 +1,45 @@
+use serde::Serialize;
+
+const PREVIEWS_DIR: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/../../dogs");
+
+#[derive(Debug, Serialize)]
+pub struct Image {
+    id: usize,
+}
+
+#[derive(Debug, Default, Serialize)]
+pub struct Images(Vec<Image>);
+
+impl Image {
+    pub fn from_id(id: usize) -> Self {
+        Self { id }
+    }
+
+    pub fn preview_path(&self) -> std::path::PathBuf {
+        let filename = format!("dog.{}.jpg", self.id);
+        let mut path = std::path::PathBuf::from(PREVIEWS_DIR);
+        path.push(filename);
+        path
+    }
+}
+
+pub fn list_images() -> Result<Images, std::io::Error> {
+    let entries = std::fs::read_dir(PREVIEWS_DIR)?;
+
+    let images = entries
+        .filter_map(|res| res.ok())
+        .filter_map(|res| {
+            let name = res.file_name();
+            parse_id(name.to_str()?)
+        })
+        .map(|id| Image::from_id(id))
+        .collect();
+
+    Ok(Images(images))
+}
+
+fn parse_id(filename: &str) -> Option<usize> {
+    let name: String = filename.chars().skip(4).take_while(|&x| x != '.').collect();
+    name.parse().ok()
+}
+
--- code/album-server/src/api.rs
+++ code/album-server/src/api.rs
@@ -1,29 +1,6 @@
+use album_db::{list_images, Image, Images};
 use rocket::{get, routes, Route};
 use rocket_contrib::json::Json;
-use serde::Serialize;
-
-const PREVIEWS_DIR: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/../../dogs");
-
-#[derive(Debug, Serialize)]
-struct Image {
-    id: usize,
-}
-
-impl Image {
-    fn from_id(id: usize) -> Self {
-        Self { id }
-    }
-
-    fn preview_path(&self) -> std::path::PathBuf {
-        let filename = format!("dog.{}.jpg", self.id);
-        let mut path = std::path::PathBuf::from(PREVIEWS_DIR);
-        path.push(filename);
-        path
-    }
-}
-
-#[derive(Debug, Default, Serialize)]
-struct Images(Vec<Image>);
 
 pub fn routes() -> Vec<Route> {
     routes![index, images, image_preview]
@@ -44,26 +21,6 @@ fn image_preview(id: usize) -> std::io::Result<Vec<u8>> {
     let image = Image::from_id(id);
     let path = image.preview_path();
     std::fs::read(path)
-}
-
-fn parse_id(filename: &str) -> Option<usize> {
-    let name: String = filename.chars().skip(4).take_while(|&x| x != '.').collect();
-    name.parse().ok()
-}
-
-fn list_images() -> Result<Images, std::io::Error> {
-    let entries = std::fs::read_dir(PREVIEWS_DIR)?;
-
-    let images = entries
-        .filter_map(|res| res.ok())
-        .filter_map(|res| {
-            let name = res.file_name();
-            parse_id(name.to_str()?)
-        })
-        .map(|id| Image::from_id(id))
-        .collect();
-
-    Ok(Images(images))
 }
 
 #[cfg(test)]
